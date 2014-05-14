596da192b9680db505ef09edcb6703c104459f6e
hurgleburgler <bubasti@gmail.com>
2014-05-14 10:27:46 -0700

    Foo

---
+++ angularize/api.py
@@ -0,0 +1,9 @@
+from tastypie.resources import ModelResource
+from tastypie import fields
+#from angularize.models import Foo 
+
+#class Resource(Mode#class Resource  class Meta:
+#        queryset = Foo.objects.all()
+#        resource_name = 'resource'
+
+
--- angularize/urls.py
+++ angularize/urls.py
@@ -1,12 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+from angularize.views import views
 
-from django.contrib import admin
-admin.autodiscover()
+# RESTful stuffs
+from tastypie.api import Api
+#from angularize.api import Resource
+from django.conf.urls import include
+
+v1_api = Api(api_name='v1')
+#v1_api.register(Resource())
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'angularize.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v1_api.urls)),
+    #url(r'^foo$', views.foo, name='foo'),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 )
